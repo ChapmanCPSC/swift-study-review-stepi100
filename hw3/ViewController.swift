e23dc4b5e71af1a5f8da34ea2ef5c6c3445a9a10
Andrew Stepien <astepien@andrews-macbook-pro-4.local>
2016-03-30 16:26:17 -0700

    committing assignment 3, study guide app

--- hw3/ViewController.swift
+++ hw3/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var t = NSTimer()
+    var counter = 0
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    var array1 = ["Let v.s. Var",
+        "Computed Initializers",
+         "Computed Variables",
+         "Setter Observers",
+         "Functions",
+         "External Parameters",
+         "Default Parameters",
+         "Anonymous Functions",
+         "Optional Chaining",
+         "Failable Initializers",
+        "Casting",
+        "Class Methods",
+        "Extensions",
+        "Enumeration",
+        "Error Handling",
+        "Dictionaries",
+        "Conditional Binding",
+        "Protocols",
+        "Inheritance",
+        "Structs",
+        "Enums",
+        "Classes",
+        "Subscripts",
+        "Setter Observers"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        label2.text=String(counter)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +51,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Next(sender: AnyObject) {
+        let count = arc4random_uniform(25)
+        label1.text=array1[Int(count)]
+        reset()
+        t = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "count", userInfo: nil, repeats: true)
+        
+    }
+
+    func count(){
+        counter += 1
+        if counter>30{
+            reset()
+        }
+        label2.text=String(counter)
+    }
+    func reset(){
+        t.invalidate()
+        counter=0
+        updatetext()
+        
+    }
+    func updatetext(){
+        let text = String(counter)
+        label2.text=text
+    
+    }
+    
 
 }
 
